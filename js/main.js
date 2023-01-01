b9ea1bfa6f76b387f6efb466d5b8f9b9c397cdfb
smile3939 <tile3939@gmail.com>
2023-01-01 18:54:27 +0900

    これからPHOTOSページ

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-const swiper = new Swiper(".mySwiper", {
+const subSwiper = new Swiper(".subSwiper", {
   cssMode: true,
   loop: true,
+  slidesPerView: 3,
+  slidesPerGroup: 3,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -11,4 +13,21 @@ const swiper = new Swiper(".mySwiper", {
   mousewheel: true,
   keyboard: true,
 });
+const swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  thumbs: {
+    swiper: subSwiper,
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
 console.log("hello2023");
