8eb151c7cd22d75e254d1c349021c18d156d73a8
smile3939 <tile3939@gmail.com>
2023-01-04 12:43:12 +0900

    アップ準備完了！

--- js/quiz.js
+++ js/quiz.js
@@ -20,10 +20,24 @@ const quiz = [
     answers: ["600角", "300角", "50角"],
     corect: "50角",
   },
+  {
+    question: "この画像の張り方の名称は？",
+    question_image: "image/yougo/yoougo_imomezi.jpg",
+    answers: ["うま目地", "イモ目地", "四半張り"],
+    corect: "イモ目地",
+  },
+  {
+    question:
+      "施工面の中心とタイルor目地の中心を合わせ、左右対称に配置されるタイルの割り付け方法の名称は？",
+    question_image: "image/yougo/yoougo_imomezi.jpg",
+    answers: ["心(しん割り)", "方追い", "目地割り"],
+    corect: "心(しん割り)",
+  },
 ];
 
 const quizLength = quiz.length;
 let quizCount = 0;
+let score = 0;
 const mondaiArea = document.querySelector(".syutudai_container");
 console.log(mondaiArea);
 const startButton = document.querySelector(".startButton");
@@ -31,30 +45,46 @@ const answerButton = document.querySelectorAll(".answerButton");
 const judge_area = document.querySelector(".judge_area");
 const nextButton = document.querySelector(".nextButton");
 const button_filter = document.querySelector(".button_filter");
+const onemoreButton = document.querySelector(".onemoreButton");
 console.log(startButton, answerButton, nextButton);
 const nextbuttoninit = () => nextButton.classList.add("none");
 nextbuttoninit();
 const filterinit = () => button_filter.classList.add("none");
 filterinit();
 
+// init
 const init = () => {
   filterinit();
 
   judge_area.firstChild.textContent = "";
   mondaiArea.innerHTML = "";
-  mondaiArea.insertAdjacentText("beforeend", `第${quizCount + 1}問`);
-  mondaiArea.insertAdjacentHTML("beforeend", "<br/>");
-  mondaiArea.insertAdjacentText("beforeend", `${quiz[quizCount].question}`);
-  console.log(`<img src=${quiz[quizCount].question_image} alt="" />`);
-  mondaiArea.insertAdjacentHTML(
-    "beforeend",
-    `<img src=${quiz[quizCount].question_image} alt="" />`
-  );
-  startButton.style.display = "none";
-  answerButton.forEach((button, index) => {
-    button.innerText = quiz[quizCount].answers[index];
-  });
+
+  if (quizCount < quizLength) {
+    mondaiArea.insertAdjacentText("beforeend", `第${quizCount + 1}問`);
+    mondaiArea.insertAdjacentHTML("beforeend", "<br/>");
+    mondaiArea.insertAdjacentText("beforeend", `${quiz[quizCount].question}`);
+    console.log(`<img src=${quiz[quizCount].question_image} alt="" />`);
+    mondaiArea.insertAdjacentHTML(
+      "beforeend",
+      `<img src=${quiz[quizCount].question_image} alt="" />`
+    );
+    startButton.style.display = "none";
+    answerButton.forEach((button, index) => {
+      button.innerText = quiz[quizCount].answers[index];
+    });
+  } else {
+    judge_area.firstChild.textContent = "";
+    mondaiArea.innerHTML = "";
+    // onemoreButton.classList.remove("none");
+    // onemoreButton.classList.add("block");
+    judge_area.firstChild.textContent = `終了！あなたの正解数は${score}／${quizLength}でした！`;
+    mondaiArea.innerHTML = `<a class="onemoreButton none" href="">
+        もう一度挑戦する
+      </a>`;
+  }
 };
+
+// init
 
 startButton.addEventListener("click", init);
 nextButton.addEventListener("click", init);
@@ -62,6 +92,7 @@ nextButton.addEventListener("click", init);
 const clickHandler = (e) => {
   if (quiz[quizCount].corect === e.target.textContent) {
     judge_area.firstChild.textContent = "正解！！";
+    score++;
   } else {
     judge_area.firstChild.textContent = `不正解！　正解は ${quiz[quizCount].corect} でした。`;
   }
